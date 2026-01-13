d4c6b4a3645d1c155f83c87982cff942ab52d231
Jaka RP <jaka.ramdhani.jtk12@gmail.com>
2026-01-13 14:52:08 +0700

    create printsNumber function

--- LogicExercise/Program.cs
+++ LogicExercise/Program.cs
@@ -2,23 +2,30 @@
 
 Console.Write("Input a number: ");
 
-if (!int.TryParse(Console.ReadLine(), out int n))
-{
-    Console.WriteLine("Invalid input. Please enter a valid integer.");
-    return;
-}
+string? input = Console.ReadLine();
 
-for (int i = 1; i <= n; i++)
-{
-    if (i > 1)
-        Console.Write(", ");
+printsNumber(ref input);
 
-    if (i % 3 == 0 && i % 5 == 0)
-        Console.Write("foobar");
-    else if (i % 3 == 0)
-        Console.Write("foo");
-    else if (i % 5 == 0)
-        Console.Write("bar");
-    else
-        Console.Write(i);
+static void printsNumber(ref string? input)
+{
+    if (!int.TryParse(input, out int parseInteger))
+    {
+        Console.WriteLine("Invalid input. Please enter a valid integer.");
+        return;
+    }
+
+    for (int i = 1; i <= parseInteger; i++)
+    {
+        if (i > 1)
+            Console.Write(", ");
+
+        if (i % 3 == 0 && i % 5 == 0)
+            Console.Write("foobar");
+        else if (i % 3 == 0)
+            Console.Write("foo");
+        else if (i % 5 == 0)
+            Console.Write("bar");
+        else
+            Console.Write(i);
+    }
 }
